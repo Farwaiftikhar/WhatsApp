f2a13e533b91470cf06cd54eec601223dc798e26
Farwaiftikhar <farwaiftikhar12@gmail.com>
2024-11-15 09:56:09 +0500

    update

--- lib/whatsapp2.dart
+++ lib/whatsapp2.dart
@@ -8,193 +8,181 @@ class WhatsApp extends StatefulWidget {
 }
 
 class _WhatsAppState extends State<WhatsApp> {
-    List  Whatsappnames = [
-      'Ayesha',
-      'Subhan',
-      'Sakina',
-      'Bisma',
-      'Hadia',
-      'Horain',
-      'Mustafa',
-      'Haider',
-      'Zahra',
-      'Filza',
-      'Aina',
-      'Saim',
-      'Sarim',
-      'Shozab',
-      'Lababa',
-      'Turab',
-      'Muqarab',
-      'Fawad',
-      'Muhammad',
-      'Moiz',
-    ];
+  List Whatsappnames = [
+    'Ayesha',
+    'Subhan',
+    'Sakina',
+    'Bisma',
+    'Hadia',
+    'Horain',
+    'Mustafa',
+    'Haider',
+    'Zahra',
+    'Filza',
+    'Aina',
+    'Saim',
+    'Sarim',
+    'Shozab',
+    'Lababa',
+    'Turab',
+    'Muqarab',
+    'Fawad',
+    'Muhammad',
+    'Moiz',
+  ];
 
+  List subtitle = [
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+    'message',
+  ];
 
-    List subtitle = [
-
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-      'message',
-
-    ];
-
-    List image = [
-     
-      'lib/assets/beauty.jpg',
-      'lib/assets/drop.jpg',
-      'lib/assets/nature.jpg',
-      'lib/assets/sky.jpg',
-      'lib/assets/stone.jpg',
-      'lib/assets/tree.jpg',
-      'lib/assets/beauty.jpg',
-      'lib/assets/drop.jpg',
-      'lib/assets/stone.jpg',
-      'lib/assets/sky.jpg',
-      'lib/assets/nature.jpg',
-      'lib/assets/drop.jpg',
-      'lib/assets/beauty.jpg',
-      'lib/assets/tree.jpg',
-      'lib/assets/stone.jpg',
-      'lib/assets/sky.jpg',
-      'lib/assets/tree.jpg',
-      'lib/assets/nature.jpg',
-      'lib/assets/beauty.jpg',
-      'lib/assets/drop.jpg',
-      
-    ];
-    
-
-
-
-
-
+  List image = [
+    'lib/assets/beauty.jpg',
+    'lib/assets/drop.jpg',
+    'lib/assets/nature.jpg',
+    'lib/assets/sky.jpg',
+    'lib/assets/stone.jpg',
+    'lib/assets/tree.jpg',
+    'lib/assets/beauty.jpg',
+    'lib/assets/drop.jpg',
+    'lib/assets/stone.jpg',
+    'lib/assets/sky.jpg',
+    'lib/assets/nature.jpg',
+    'lib/assets/drop.jpg',
+    'lib/assets/beauty.jpg',
+    'lib/assets/tree.jpg',
+    'lib/assets/stone.jpg',
+    'lib/assets/sky.jpg',
+    'lib/assets/tree.jpg',
+    'lib/assets/nature.jpg',
+    'lib/assets/beauty.jpg',
+    'lib/assets/drop.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          title: Text('WhatsApp', style: TextStyle(
-              color: Colors.green, fontSize: 30, fontWeight: FontWeight.bold),),
-          actions: [
-            IconButton(
-              onPressed: () {}, icon: Icon(Icons.camera_alt_outlined),),
-            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-          ],
-
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'WhatsApp',
+          style: TextStyle(
+              color: Colors.green, fontSize: 30, fontWeight: FontWeight.bold),
         ),
-
-
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Expanded(
-             // flex: 1,
-            Container(
-              margin: EdgeInsets.only(left: 40,top: 10,right: 20, bottom: 10),
-              alignment: Alignment.center,
-              height: 55,
-              width: 350,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(color: Colors.grey),
-              ),
-
-
-
-              child:
-              ListTile(
-                leading: Icon(
-                  Icons.circle_outlined, color: Colors.purpleAccent, size: 35,),
-                title: Text(
-                  'Ask Meta AI or Search', style: TextStyle(fontSize: 18),),
-              ),
-            ),
-
-
-            Expanded(
-              flex: 10,
-              child:
-              ListView.builder(
-                itemCount: Whatsappnames.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: AssetImage(image[index]),
-                    ),
-                    title: Text(Whatsappnames[index]),
-                    subtitle: Text(subtitle[index]),
-                    trailing: Text('2:34 PM'),);
-                },
-              ),
-            ),
-
-            Expanded(
-              flex: 1,
-              child:
-              BottomNavigationBar(
-                items: [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.chat,),
-                      label: 'Chats'
-                  ),
-
-
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.update),
-                    label: 'Updates',
-                  ),
-
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.call),
-
-                      label: 'Calls'
-                  ),
-
-
-                ],),
-            ),],     ),
-
-
-
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.camera_alt_outlined),
+          ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          FloatingActionButton(
-            child: Icon(
-              Icons.circle_outlined, color: Colors.purpleAccent, size: 30,),
-            onPressed: () {},),
+          //Expanded(
+          // flex: 1,
+          Container(
+            margin: EdgeInsets.only(left: 40, top: 10, right: 20, bottom: 10),
+            alignment: Alignment.center,
+            height: 55,
+            width: 350,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              border: Border.all(color: Colors.grey),
+            ),
+            child: ListTile(
+              leading: Icon(
+                Icons.circle_outlined,
+                color: Colors.purpleAccent,
+                size: 35,
+              ),
+              title: Text(
+                'Ask Meta AI or Search',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
 
-          SizedBox(height: 10,),
+          Expanded(
+            flex: 10,
+            child: ListView.builder(
+              itemCount: Whatsappnames.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage(image[index]),
+                  ),
+                  title: Text(Whatsappnames[index]),
+                  subtitle: Text(subtitle[index]),
+                  trailing: Text('2:34 PM'),
+                );
+              },
+            ),
+          ),
 
-          FloatingActionButton(
+          Expanded(
+            flex: 1,
+            child: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.chat,
+                    ),
+                    label: 'Chats'),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.update),
+                  label: 'Updates',
+                ),
+                BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Calls'),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Stack(
+      //Column(
+       // mainAxisAlignment: MainAxisAlignment.end,
+        //crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 70,bottom: 70,right: 10),
+       child: FloatingActionButton(child: Icon(Icons.add), onPressed: () {})),
+         SizedBox(
+            height: 5,
+          ),
+          Container(
 
-              child: Icon(Icons.add),
-              onPressed: () {}),
+    child: FloatingActionButton(
+    child: Icon(
+    Icons.circle_outlined,
+    color: Colors.purpleAccent,
+    size: 30,
+    ),
+    onPressed: () {},
+    ),
 
-        ],),);
-
+          ), ],
+      ),
+    );
   }
 }
